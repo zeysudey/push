8c3641b58949138dc61ed8654eab8abb2fb7f197
Zeynep Sude Yılmaz <97186117+zeysudey@users.noreply.github.com>
2025-02-20 13:47:35 +0300

    Update ft_index.c

--- ft_index.c
+++ ft_index.c
@@ -12,11 +12,60 @@
 
 #include "push_swap.h"
 
+static void	reset_i(t_list **a)
+{
+	t_list	*current;
+
+	if (!a || !(*a))
+		return;//burda free bak
+	current = *a;
+	while (current)
+	{
+		current->index = -1; // Başlangıçta tüm indeksleri -1 yap
+		current = current->next;
+	}
+}
+
+static t_list	*find_min(t_list **a)
+{
+	t_list	*current;
+	t_list	*min_node;
+	int		min_val;
+
+	min_val = 2147483647; // INT_MAX
+	min_node = NULL;
+	current = *a;
+	while (current)
+	{
+		if (current->index == -1 && current->val < min_val)
+		{
+			min_val = current->val;
+			min_node = current;
+		}
+		current = current->next;
+	}
+	return (min_node);
+}
 
 void ft_index (t_list **a)
 {
-    int i;
-    t_list *a_index;
-    
+    t_list	*min_node;
+    int		i;
+	int		list_len;
+
+    if (!a || !(*a))
+		return;
+    reset_i(a);
+	i = 0;
+	list_len = list_size(*a); // Listenin boyutunu al
+
+	while (current_index < list_len)
+	{
+		min_node = find_min(a);
+		if (!min_node)
+			break;
+		min_node->index = i;
+		i++;
+    }
     
 }
